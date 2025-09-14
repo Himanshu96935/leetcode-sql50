1d8c24347e150ee7d143a1996b5c2bc3e0f3c25d
Himanshu Raj <130293520+Himanshu96935@users.noreply.github.com>
2025-09-14 17:53:58 +0530

    Create 02_find_customer_referee.sql

---
+++ easy/02_find_customer_referee.sql
@@ -0,0 +1,6 @@
+-- Problem: Find Customer Referee
+-- Source: https://leetcode.com/problems/find-customer-referee/
+-- Difficulty: Easy
+SELECT name
+FROM Customer
+WHERE referee_id <> 2 OR referee_id IS NULL;
